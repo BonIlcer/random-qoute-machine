cce66bcdae85b90e4c7a8350fec1c08be9b66ce3
BonIlcer <pantaev.e@mail.ru>
2020-09-09 21:24:25 +0700

    Add styles

--- src/Quote-box.js
+++ src/Quote-box.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import twitterLogo from "./twitter-brands.svg";
 
-function openURL(url) {
-  window.open(url);
-}
-
-function inFrame() {
-  try {
-    return window.self !== window.top;
-  } catch (e) {
-    return true;
-  }
-}
+const colors = [
+  "#16a085",
+  "#27ae60",
+  "#7692FF",
+  "#513C2C",
+  "#FFADCA",
+  "#E6D3A3",
+  "#61E786",
+  "#39283E",
+  "#00171F",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857",
+];
 
 function QuoteBox() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
-  const [curQuote, setQuote] = useState(0);
+  const [curQuoteIndex, setQuoteIndex] = useState(0);
+  const [curColor, setColor] = useState(colors[0]);
 
   function randomQuote() {
     const index = Math.floor(items.length * Math.random());
@@ -44,6 +54,11 @@ function QuoteBox() {
       );
   }, []);
 
+  useEffect(() => {
+    console.log("Current color: " + curColor);
+    document.body.style.backgroundColor = curColor;
+  }, [curColor]);
+
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
@@ -51,26 +66,53 @@ function QuoteBox() {
   } else {
     return (
       <div id="quote-box">
-        <div id="text">{items[curQuote].quote}</div>
-        <div id="author">{items[curQuote].author}</div>
+        <div id="text" style={{ color: curColor }}>
+          {items[curQuoteIndex].quote}
+        </div>
+        <div id="author" style={{ color: curColor }}>
+          {"— " + items[curQuoteIndex].author}
+        </div>
         <div className="buttons">
           <a
-            onClick={() => {
-              openURL(
-                "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
-                  encodeURIComponent(
-                    '"' + items[curQuote].quote + '" ' + items[curQuote].author
-                  )
-              );
-            }}
+            id="tweet-quote"
+            className="button"
+            title="Tweet this quote!"
+            href={
+              "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+              encodeURIComponent(
+                '"' +
+                  items[curQuoteIndex].quote +
+                  '" ' +
+                  items[curQuoteIndex].author
+              )
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ backgroundColor: curColor }}
           >
-            <img className="logo" src="" alt="twitter logo" />
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="twitter"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="white"
+                d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
+              ></path>
+            </svg>
           </a>
           <button
+            className="button"
             id="new-quote"
             onClick={() => {
-              setQuote(randomQuote());
+              setColor(colors[Math.floor(colors.length * Math.random())]);
+              setQuoteIndex(randomQuote());
             }}
+            style={{ backgroundColor: curColor }}
           >
             New quote
           </button>
